5ac33f34cb1242b1ea58b807b0b55b1b42fd4143
Hemant Pema <hemant@pema.co.za>
2017-12-20 16:36:25 +0200

    update

--- crypto/crypto/doctype/coin/coin.py
+++ crypto/crypto/doctype/coin/coin.py
@@ -34,29 +34,10 @@ def update_coins_rate():
                 exists.zar_price = coin['price_zar']
                 exists.save()
         last_run.last_polled_list=frappe.utils.now()
-        last_run.save()
-        frappe.db.commit()
+        #last_run.save()
+        #frappe.db.commit()
 
     if frappe.utils.time_diff_in_seconds(frappe.utils.now(),last_run.last_polled_rates)>10:
-        coins = frappe.get_all('Coin')
-        for coin in coins:
-            exists = frappe.get_doc('Coin',coin['name'])
-            api_url = "https://min-api.cryptocompare.com/data/price?fsym={0}&tsyms=USD,ZAR".format(exists.symbol)
-            response = requests.get(api_url)
-            r= json.loads(response.text)
-            #return r["USD"]
-            try:
-                exists.usd_price = r["USD"]
-                exists.zar_price = r["ZAR"]
-            except:
-                exists.usd_price = 0
-                exists.zar_price = 0
-            exists.save()
-
-            last_run.last_polled_rates=frappe.utils.now()
-            last_run.save()
-            frappe.db.commit()
-
         wallets = frappe.get_all('Wallet')
         for w in wallets:
             wallet = frappe.get_doc('Wallet', w['name'])
@@ -64,17 +45,32 @@ def update_coins_rate():
             wallet.total = 0
             wallet.total_coins = 0
             wallet.user = wallet.owner
+            coin = frappe.get_doc('Coin', wallet.coin)
+            if coin:
+                api_url = "https://min-api.cryptocompare.com/data/price?fsym={0}&tsyms=USD,ZAR".format(coin.symbol)
+                response = requests.get(api_url)
+                r= json.loads(response.text)
+                #return r["USD"]
+                try:
+                    coin.usd_price = r["USD"]
+                    coin.zar_price = r["ZAR"]
+                except:
+                    coin.usd_price = 0
+                    coin.zar_price = 0
+                coin.save()
+
             for line in wallet.transactions:
                 line.profit_or_loss=0
                 wallet.total_coins = wallet.total_coins + line.quantity
-                coin = frappe.get_doc('Coin', line.coin)
-                if coin:
-                    wallet.total = wallet.total + line.spent
-                    if line.quantity >0:
+                wallet.total = wallet.total + line.spent
+                if line.quantity >0:
                     line.profit_or_loss = (coin.zar_price*line.quantity) - line.spent
-                    wallet.profit_or_loss = wallet.profit_or_loss + line.profit_or_loss
+                wallet.profit_or_loss = wallet.profit_or_loss + line.profit_or_loss
                 line.save()
             wallet.save()
-            frappe.db.commit()
 
-    return last_run.last_polled
+        last_run.last_polled_rates=frappe.utils.now()
+        last_run.save()
+        frappe.db.commit()
+        
+    return [last_run.last_polled_rates,last_run.last_polled_list]
